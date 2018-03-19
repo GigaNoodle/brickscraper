821b29d64d7785fc33dfdbbbb3cba9cbdc9d8be0
GigaNoodle <32640629+GigaNoodle@users.noreply.github.com>
2018-03-19 01:41:40 -0700

    Initial commit.

---
+++ brickscraper.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class BrickSetSpider(scrapy.Spider):
+    name = "brickset_spider"
+    start_urls = ['http://brickset.com/sets/year-2016']
+
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        #infile1 = open("scrapedata.txt", "w")
+
+        for brickset in response.css(SET_SELECTOR):
+            
+            NAME_SELECTOR = 'h1 ::text'
+            PIECE_SELECTOR = 'dd ::text'
+            yield {
+                'name': brickset.css(NAME_SELECTOR).extract_first(),
+                'pieces': brickset.css(PIECE_SELECTOR).extract_first(),
+
+            }
+            #infile1.write()
+            #infile1.close()
+            #infile1.write(NAME_SELECTOR)
+            #infile1.write(PIECE_SELECTOR)
